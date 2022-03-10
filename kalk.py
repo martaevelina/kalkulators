2759ddc8e3136542ad73a5a6f954e65446cf069e
martaevelina <95850252+martaevelina@users.noreply.github.com>
2022-03-10 11:05:40 +0200

    Add files via upload

--- kalk.py
+++ kalk.py
@@ -9,13 +9,16 @@ while turpinat == True:
     6 - kilometri uz jūdzēm
     7 - kubikcentimetri  uz litriem
     8 - litri uz kubikcentimetriem
-    9 - eiro uz ASV dolāriem
-    10 -ASV dolāri uz eiro""")
-    izvele = int(input(""))
+    9 - kg/m3 uz g/ml
+    10 - g/ml uz kg/m3
+    11- eiro uz ASV dolāriem
+    12 -ASV dolāri uz eiro""")
+    izvele = float(input(""))
 
     if izvele == 1:
-        kgs = int(input("Ievadi kilogramus: "))
+        kgs = float(input("Ievadi kilogramus: "))
         lbs = kgs * 2.205
+        lbs = round(lbs, 2)
         print(f"{kgs} kilogrami ir {lbs} mārciņas")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NE: ")
@@ -24,8 +27,9 @@ while turpinat == True:
             turpinat = False
 
     if izvele == 2:
-        lbs = int(input("Ievadi mārciņas: "))
+        lbs = float(input("Ievadi mārciņas: "))
         kgs = lbs * 0.4536
+        kgs = round(kgs, 3)
         print(f"{lbs} mārciņas ir {kgs} kilogrami")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -34,8 +38,9 @@ while turpinat == True:
             turpinat = False
     
     if izvele == 3:
-        inch = int(input("Ievadi collas: "))
+        inch = float(input("Ievadi collas: "))
         cm = inch * 2.54
+        cm = round(cm, 2)
         print(f"{inch} collas ir {cm} centimetri")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -44,8 +49,9 @@ while turpinat == True:
             turpinat = False
 
     if izvele == 4:
-        cm = int(input("Ievadi centimetrus: "))
+        cm = float(input("Ievadi centimetrus: "))
         inch = cm * 0.3937
+        inch = round(inch, 2)
         print(f"{cm} centimetri ir {inch} collas")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -54,8 +60,9 @@ while turpinat == True:
             turpinat = False
     
     if izvele == 5:
-        mi = int(input("Ievadi jūdzes: "))
+        mi = float(input("Ievadi jūdzes: "))
         km = mi * 1.60934
+        km = round(km, 2)
         print(f"{mi} jūdzes ir {km} kilometri")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -64,8 +71,9 @@ while turpinat == True:
             turpinat = False
     
     if izvele == 6:
-        km =  int(input("Ievadi kilometrus: "))
+        km =  float(input("Ievadi kilometrus: "))
         mi = km * 0.621371
+        mi = round(mi, 2)
         print(f"{km} kilometri ir {mi} jūdzes")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -75,8 +83,9 @@ while turpinat == True:
     
 
     if izvele == 7:
-        cm3 =  int(input("Ievadi kubikcentimetrus: "))
+        cm3 =  float(input("Ievadi kubikcentimetrus: "))
         l = cm3 * 0.001
+        l = round(l, 2)
         print(f"{cm3} kubikcentimetri ir {l} litri")
 
         izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
@@ -84,3 +93,49 @@ while turpinat == True:
         if izvele.lower() == "nē" or izvele.lower() == "ne":
             turpinat = False
     
+    if izvele == 8:
+        l = float(input("Ievadi litrus: "))
+        cm3 = l * 1000
+        cm3 = round(cm3, 2)
+        print(f"{l} litri ir {cm3} kubikcentimetri")
+
+        izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
+
+        if izvele.lower() == "nē" or izvele.lower() == "ne":
+            turpinat = False
+
+    if izvele == 9:
+        kg_m3 = float(input("Ievadi kg/m3: "))
+        g_ml = kg_m3 * 0.001
+        cm3 = round(cm3, 2)
+        print(f"{l} litri ir {cm3} kubikcentimetri")
+
+        izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
+
+        if izvele.lower() == "nē" or izvele.lower() == "ne":
+            turpinat = False
+
+    if izvele == 11:
+        eiro = float(input("Ievadi Eiro: "))
+        usd = eiro * 1.1043
+        usd = round(usd, 2)
+        print(f"{eiro} Eiro ir {usd} dolāri.")
+
+        izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
+
+        if izvele.lower() == "nē" or izvele.lower() == "ne":
+            turpinat = False
+
+    if izvele == 12:
+        usd = float(input("Ievadi dolārus: "))
+        eiro = usd * 0.9055
+        eiro = round(eiro, 2)
+        print(f"{usd} Dolāri ir {eiro} eiro.")
+
+        izvele = input("Vai tu vēlies turpināt? JĀ/NĒ: ")
+
+        if izvele.lower() == "nē" or izvele.lower() == "ne":
+            turpinat = False
+        
+
+
